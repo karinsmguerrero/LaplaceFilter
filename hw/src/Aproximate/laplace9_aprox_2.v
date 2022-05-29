e7013f7d74c61587d2762e0134be9c884cb14fb1
Karina <karinamguerrero@estudiantec.cr>
2022-05-28 20:09:57 -0600

    aproximados Lenna listos

---
+++ hw/src/Aproximate/laplace9_aprox_2.v
@@ -0,0 +1,30 @@
+module laplace9_aprox_2 (
+	input [7:0] b,
+	input [7:0] d,
+	input [7:0] e,
+	input [7:0] f,
+	input [7:0] h,
+	output [7:0] s
+);
+	wire cout, cout1, cout2, cout3, cout4;
+	wire [7:0] res1, res2, res3, res4, epor4, laplace;
+	wire [9:0] sum;
+
+	// b + d -> 9 b
+    LSB_Three_AproximateRCAdder rc1 (b, d, 1'b0, res1, cout1);
+	// f + h -> 9 b
+    LSB_Three_AproximateRCAdder rc2 (f, h, 1'b0, res2, cout2);
+	//(b + d) + (f + h) -> 9 b
+    LSB_Three_AproximateRCAdder rc3 (res1, res2, 1'b0, res3, cout3);
+	FullAdder fa1 (cout1, cout2, cout3, cout, cout4);
+	//(b + d + f + h) -> 10 b
+    assign sum = {cout4, cout, res3};
+	assign epor4 = {e[5:0], 2'b00};
+	assign laplace = sum - epor4;
+	assign s = (laplace > 255) ? 255 : (laplace < 0) ? 0 : laplace;
+	
+endmodule
+
+
+
+
---
+++ hw/src/Aproximate/laplace9_aprox_3_tb.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 10ps
+
+module laplace9_aprox_3_tb();
+
+parameter ROWS = 510;
+parameter COLS = 510;
+
+	reg [7:0] b;
+	reg [7:0] d;
+    reg [7:0] e;
+    reg [7:0] f;
+    reg [7:0] h;
+	wire [7:0] s;
+
+	integer row,col;
+	integer file;
+	reg [7:0] pixel = 0;
+	
+	reg [7:0] img [0:262143];
+
+	laplace9_aprox_3 DUT (.b(b), .d(d), .e(e), .f(f), .h(h), .s(s));
+
+	initial begin
+		$display("--- Beginning simulation ---");
+		
+		$readmemb("../../sw/image.txt", img);
+		file = $fopen("imageFiltered_aprox_3.txt","w");
+
+		for(row = 0; row < ROWS; row = row + 1) begin
+			for (col = 0; col < COLS; col = col + 1) begin
+				b = img[(col + 1) + (row * 512)]; 
+				d= img[col + ((row + 1) * 512)];
+				e= img[(col + 1) + ((row + 1) * 512)];
+				f= img[(col + 2) + ((row + 1) * 512)];
+				h= img[(col + 1) + ((row + 2) * 512)];
+				#10
+				$fwrite(file,"%b\n", s);
+			end
+		end
+		
+		$fclose(file);
+		$display("-- Ending simulation --");
+		$finish;
+	end
+
+endmodule
